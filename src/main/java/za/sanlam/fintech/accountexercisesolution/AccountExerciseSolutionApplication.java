132e313a22222bc91931decd6210b6f25f792bc6
evanskfchikuni <vance04real@gmail.com>
2025-02-26 21:12:38 +0200

    init

---
+++ src/main/java/za/sanlam/fintech/accountexercisesolution/AccountExerciseSolutionApplication.java
@@ -0,0 +1,13 @@
+package za.sanlam.fintech.accountexercisesolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountExerciseSolutionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountExerciseSolutionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/za/sanlam/fintech/accountexercisesolution/AccountExerciseSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package za.sanlam.fintech.accountexercisesolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountExerciseSolutionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
